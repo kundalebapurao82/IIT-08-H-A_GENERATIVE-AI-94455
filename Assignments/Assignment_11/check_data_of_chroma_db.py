f6ef9f737b933ba98242c750e496c1fa517f47e0
Bapurao Anil Kundale <bapurao2004@gmail.com>
2025-12-27 12:57:43 +0530

    Assignment 11 Completed

--- Assignments/Assignment_11/check_data_of_chroma_db.py
+++ Assignments/Assignment_11/check_data_of_chroma_db.py
@@ -5,7 +5,7 @@ collection = db.get_or_create_collection("resumes")
 
 
 all_data = collection.get()
-print("All data: ", all_data["ids"])
+print("All data: ", all_data["metadatas"])
 
 
 # print("Count of data in collection: ", collection.count())
